8017fcd3f2dabe885abe25cfad2c7d9113ec58d9
Xabier Figueroa <xabier.figueroa@ingenoba.com>
2024-09-02 13:04:22 +0200

    Navigation to quiz fragment and design the layout for it

--- app/src/main/java/com/xabierf/quizland/view/MainActivity.kt
+++ app/src/main/java/com/xabierf/quizland/view/MainActivity.kt
@@ -6,15 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.xabierf.quizland.R
-import com.xabierf.quizland.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var _binding : ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
